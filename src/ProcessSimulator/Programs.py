0c4a1f4a8332ec319f1e95874a187a4197929e0b
Mateus Pinto da Silva <mateus.p.silva@ufv.br>
2020-10-24 11:08:33 -0300

    fork and replace done in programs

--- src/ProcessSimulator/Programs.py
+++ src/ProcessSimulator/Programs.py
@@ -7,9 +7,61 @@ class ProgramList(object):
 
 class TextSection(object):
     """
-    The text section of a program. Contains a list of instructions
+    The text section of a program. Contains a list of instructions.
     """
-    pass
+
+    def __read_file__(self, fileNumber: int, limit: bool = False, startLine: int = 0, endLine: int = 0):
+        self.code = []
+        self.fileNumber = fileNumber
+
+        with open("programs/" + str(fileNumber) + ".txt") as file:
+
+            if fileNumber < 0:
+                raise NameError("fileNumber must be a unsigned int!")
+
+            for line_number, line in enumerate(file, 1):
+
+                if not limit or startLine <= line_number <= endLine:
+
+                    try:
+                        opcode = str(line.strip().split(' ')[0])
+                    except:
+                        raise NameError(
+                            "Wrong syntax! Bad opcode! Error on line " + str(line_number))
+
+                    try:
+                        n = int(line.strip().split(' ')[1])
+                    except:
+                        n = 0
+
+                    try:
+                        x = int(line.strip().split(' ')[2])
+                    except:
+                        x = 0
+
+                    try:
+                        self.code.append(Instruction(opcode, n, x))
+                    except:
+                        raise NameError(
+                            "Wrong syntax! Error on line = " + str(line_number))
+
+    def __init__(self, fileNumber: int, limit: bool = False, startLine: int = 0, endLine: int = 0):
+        self.__read_file__(fileNumber, limit, startLine, endLine)
+
+    def __str__(self):
+        display: str = ''
+
+        for instruction in self.code:
+            display += str(instruction) + '\n'
+
+        return display[:-1]
+
+    def fork(self, fromLine: int, howManyLines: int):
+        self.__read_file__(self.fileNumber, True, fromLine,
+                           fromLine + howManyLines - 1)
+
+    def replace(self, newFileNumber: int):
+        self.__read_file__(newFileNumber)
 
 
 class Instruction(object):
@@ -19,9 +71,23 @@ class Instruction(object):
 
     def __init__(self, opcode: str, n: int, x: int):
 
-        if not (opcode == ('N' or 'D' or 'V' or 'A' or 'S' or 'B' or 'T' or 'F' or 'R')) or n < 0:
+        if not (opcode == 'N' or opcode == 'D' or opcode == 'V' or opcode == 'A' or opcode == 'S' or opcode == 'B' or opcode == 'T' or opcode == 'F' or opcode == 'R'):
             raise NameError("Opcode is not valid!")
+
+        if n < 0:
+            raise NameError("N must be an unsigned int!")
 
         self.opcode = opcode
         self.n = n
         self.x = x
+
+    def __str__(self):
+        display: str = self.opcode
+
+        if not (self.opcode == 'B' or self.opcode == 'T'):
+            display += ' ' + str(self.n)
+
+            if not (self.opcode == 'N' or self.opcode == 'D' or self.opcode == 'F' or self.opcode == 'R'):
+                display += ' ' + str(self.x)
+
+        return display
--- src/main.py
+++ src/main.py
@@ -1,3 +1,12 @@
 from ProcessSimulator.Programs import *
 
-Instruction("asdasd", 3, 3)
+a = TextSection(0)
+print(a)
+
+print("Fork da primeira instrução até a segunda")
+a.fork(1,2)
+print(a)
+
+print("Replace por 1")
+a.replace(1)
+print(a)
